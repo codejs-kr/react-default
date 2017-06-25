a769882cc221f2a26ae7a7b964ba98d0fd01c193
dodortus <dodortus@gmail.com>
2017-06-25 16:08:56 +0900

    * add setting

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: __dirname + '/public/',
+    filename: 'bundle.js'
+  },
+
+  devServer: {
+    hot: true,    // reloading
+    inline: true, // include devServer to bundle
+    host: '0.0.0.0',
+    port: 4000,
+    contentBase: __dirname + '/public/'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        // react-hot-loader은 수정시 state 유지 시켜준다.
+        loaders: ['react-hot-loader', 'babel-loader?' + JSON.stringify({
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        })],
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
